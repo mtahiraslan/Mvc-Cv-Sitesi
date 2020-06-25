183353b27717ffed17f0c358f6221f0b8b5a6071
Mehmet Tahir Aslan <mtahiraslan23@gmail.com>
2020-06-25 19:36:26 +0300

    Asp.Net Mvc İle Cv (Öz Geçmiş ) Web Sitesi
    .Net Mvc yapısı kullnılarak MS-SQL Server veritabanı ile hazırlanmış bir cv web sitesidir.
    
    Program kullanıcı tarafında, kullanıcı giriş, üye olma,  kendi özgeçmişini sitede görebilme ve kullanıcı-şifre işlemlerini değiştirebildiği yapıları içermektedir.
    
    Admin tarafında ise, admin panelini görüntüleme,  (eğitim hayatı, hobi, hakkımda, konferans & sertifika, yetenek,deneyim) ekleme-güncelleme-silme gibi yapıları içermektedir.

---
+++ ProjecCV/ProjecCV/Controllers/SecurityController.cs
@@ -0,0 +1,53 @@
+﻿using ProjecCV.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ProjecCV.Controllers
+{
+    [AllowAnonymous]
+   
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(TBLUSERS kullanici)
+        {
+            var kullaniciInDb = db.TBLUSERS.FirstOrDefault(x => x.USERNAME == kullanici.USERNAME && x.PASSWORD == kullanici.PASSWORD);
+            if (kullaniciInDb!=null)
+            {
+                if (kullaniciInDb.ROLE == "admin")
+                {
+                    FormsAuthentication.SetAuthCookie(kullaniciInDb.USERNAME, false);
+                    return RedirectToAction("Index", "Hakkimda");
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(kullaniciInDb.USERNAME, false);
+                    return RedirectToAction("Index","Default");
+                }
+                
+            }
+            else
+            {
+                ViewBag.Mesaj = "Geçersiz Kullanıcı Adı veya Şifre";
+                return View();
+            }
+            
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ ProjecCV/ProjecCV/Controllers/YeteneklerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjecCV.Models.Entity;
+using ProjecCV.Models.Sinif;
+
+namespace ProjecCV.Controllers
+{
+    public class YeteneklerController : Controller
+    {
+        // GET: Yetenekler
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger4 = db.TBLSKILLS.ToList();
+            return View(cs);
+        }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(TBLSKILLS p)
+        {
+            db.TBLSKILLS.Add(p);
+            db.SaveChanges();
+            return View(p);
+        }
+        public ActionResult Sil(int id)
+        {
+            var yetenek = db.TBLSKILLS.Find(id);
+            db.TBLSKILLS.Remove(yetenek);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YetenekGetir(int id)
+        {
+            var yetenek = db.TBLSKILLS.Find(id);
+            return View("YetenekGetir", yetenek);
+        }
+        public ActionResult Guncelle(TBLSKILLS p)
+        {
+            var yetenekler = db.TBLSKILLS.Find(p.ID);
+            yetenekler.SKILL = p.SKILL;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
